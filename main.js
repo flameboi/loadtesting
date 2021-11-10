2238286450fcc4c496a249ae77d1189261443945
param <66104707+flameboi@users.noreply.github.com>
2021-11-10 10:47:08 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+{script type="text/javascript" src="/Client/public/core.js"></script}
+import http from 'k6/http';
+import { sleep } from 'k6';
+// init
+export let options = {
+vus: 5,
+duration: '60s',
+};
+// vu script
+export default function () 
+{
+let res = http.get('http://test.k6.io');
+console.log(res.status);
+sleep(5);
+}
